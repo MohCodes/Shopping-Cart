e6b147b05118e5e2ce87e601d422c7d67200326d
moefarts <moefarts@my.yorku.ca>
2022-02-03 19:08:22 -0500

    z

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route,useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Products from "./Components/Products.js"
 import Home from "./Components/Home.js"
